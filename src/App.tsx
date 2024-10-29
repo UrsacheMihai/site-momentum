058ecc7db546465d0127802e394bcffa75909b5d
UrsacheMihai <0ursachemihaiandrei0@gmail.com>
2024-10-30 00:01:49 +0200

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,22 @@ import {Box, Button, Paper, Stack, Typography} from "@mui/material";
 function App() {
     return (
         <Box paddingTop={"25px"}>
-            <Stack direction={"row"} spacing={"1rem"} position={"sticky"} top={0}>
+            <Stack direction={"row"} spacing={"1rem"} position={"sticky"} paddingTop={"25px"} top={0}>
                 <Button variant={"contained"}>Home</Button>
                 <Button variant={"contained"}>Info</Button>
             </Stack>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
             <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
         </Box>
     );
